2f7d2287ae5cfc8a0f1026caa0463862122546b9
merconzo <75909944+merconzo@users.noreply.github.com>
2023-06-12 21:09:43 +0300

    cross_validate.py

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -6,7 +6,8 @@ from IMLearn import BaseEstimator
 
 
 def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
-                   scoring: Callable[[np.ndarray, np.ndarray, ...], float], cv: int = 5) -> Tuple[float, float]:
+                   scoring: Callable[[np.ndarray, np.ndarray, ...], float],
+                   cv: int = 5) -> Tuple[float, float]:
     """
     Evaluate metric by cross-validation for given estimator
 
@@ -37,4 +38,20 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    indices = np.arange(X.shape[0])
+    sub_locs = np.array_split(indices, cv)
+
+    train_score, valid_score = 0.0, 0.0
+    for locs in sub_locs:
+        i_train = np.setdiff1d(indices, locs)
+        cur_train_X, cur_train_y = X[i_train], y[i_train]
+        fitted_copy = deepcopy(estimator).fit(cur_train_X, cur_train_y)
+        cur_pred_y = fitted_copy.predict(cur_train_X)
+        train_score += scoring(cur_train_y, cur_pred_y)
+
+        cur_X, cur_y = X[locs], y[locs]
+        cur_pred_y = fitted_copy.predict(cur_X)
+        valid_score += scoring(cur_y, cur_pred_y)
+
+    return (train_score / cv), (valid_score / cv)
+
